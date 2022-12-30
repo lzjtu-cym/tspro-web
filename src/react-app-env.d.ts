cec2f0668acc5390486961398e359d56dacf8c6b
lzjtu_cym <1601561783@qq.com>
2022-12-30 18:18:28 +0800

    d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,31 @@
 /// <reference types="react-scripts" />
+/// <reference types="react-router-dom" />
 declare module '*.less' {
   const content: { [className: string]: string };
   export default content;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
 }
